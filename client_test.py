1dc9578f21d88c8dfe826e15a12700c4cb88b0b6
TJWRii <s1912558@ed.ac.uk>
2024-06-06 17:10:29 +0100

    add unit test

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock = quote['stock']
+      bid_price = quote['top_bid']['price']
+      ask_price = quote['top_ask']['price']
+      price = (bid_price + ask_price) / 2
+      self.assertEqual(getDataPoint(quote), (stock, bid_price, ask_price, price))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +21,20 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock = quote['stock']
+      bid_price = quote['top_bid']['price']
+      ask_price = quote['top_ask']['price']
+      price = (bid_price + ask_price) / 2
+      self.assertEqual(getDataPoint(quote), (stock, bid_price, ask_price, price))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_divideByZero(self):
+    self.assertIsNone(getRatio(100, 0))
+  
+  def test_getRatio_calculateRatio(self):
+        self.assertEqual(getRatio(10, 2), 5)
 
 
 
